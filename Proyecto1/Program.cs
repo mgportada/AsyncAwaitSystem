d01fc1271a2d5396037d5c594a742f9c24a5ee51
manugp <manuelgarces@iesportada.org>
2024-05-27 10:53:04 +0200

    Agregar archivos de proyecto.

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime inicio = DateTime.Now;
+            Console.WriteLine("¡Hora de cenar!");
+
+            Person alice = new Person();
+            Burger burger = alice.PedirBurger();
+            alice.jugarPlay();
+            alice.Comer(burger);
+
+
+            DateTime fin = DateTime.Now;
+            TimeSpan tiempoTranscurrido = fin - inicio;
+            Console.WriteLine("Tiempo total transcurrido: " + tiempoTranscurrido);
+        }
+
+    }
+
+    class Hamburgueseria
+    {
+        public static Burger PrepararPedido()
+        {
+            Burger burger = new Burger();
+            Console.WriteLine("Burger preparandose... (3seg)");
+            Thread.Sleep(3000);
+            Console.WriteLine("Burger enviandose... (3seg)");
+            Thread.Sleep(3000);
+            Console.WriteLine("Burger llego!");
+            return burger;
+        }
+    }
+
+    class Person()
+    {
+        public Burger PedirBurger()
+        {
+            Console.WriteLine("Realizando llamada!");
+            Burger burger = Hamburgueseria.PrepararPedido();
+            return burger;
+        }
+
+        public void Comer(Burger burger)
+        {
+            Console.WriteLine($"Comiendo {burger.Tipo}... (1seg)");
+            Thread.Sleep(1000);
+        }
+
+        public void jugarPlay()
+        {
+            Console.WriteLine("Jugando Videojuegos... (5seg)");
+            Thread.Sleep(5000);
+        }
+    }
+
+
+    class Burger
+    {
+        public string Tipo { get; set; }
+        public Burger()
+        {
+            Tipo = "Cheese Burger";
+        }
+    }
+
+}
---
+++ Proyecto2/Program.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Proyecto2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DateTime inicio = DateTime.Now;
+            Console.WriteLine("¡Hora de cenar!");
+
+            Person alice = new Person();
+            Task<Burger> burgerTask = alice.PedirBurgerAsync();
+            alice.JugarPlay();
+            Burger burger = await burgerTask;
+            alice.Comer(burger);
+
+            DateTime fin = DateTime.Now;
+            TimeSpan tiempoTranscurrido = fin - inicio;
+            Console.WriteLine("Tiempo total transcurrido: " + tiempoTranscurrido);
+        }
+    }
+
+    class Person
+    {
+        public async Task<Burger> PedirBurgerAsync()
+        {
+            Console.WriteLine("Realizando llamada...");
+            Burger burger = await Hamburgueseria.PrepararPedido();
+            return burger;
+        }
+
+        public void Comer(Burger burger)
+        {
+            Console.WriteLine($"Comiendo {burger.Tipo}... (1seg)");
+            Thread.Sleep(1000);
+        }
+
+        public void JugarPlay()
+        {
+            Console.WriteLine("Jugando Videojuegos... (5seg)");
+            Thread.Sleep(5000);
+            Console.WriteLine("Fin de la Partida!");
+        }
+    }
+
+    class Hamburgueseria
+    {
+        public static async Task<Burger> PrepararPedido()
+        {
+            Burger burger = new Burger();
+            Console.WriteLine("Burger preparándose... (3seg)");
+            await Task.Delay(3000);
+            Console.WriteLine("Burger enviándose... (3seg)");
+            await Task.Delay(3000);
+            Console.WriteLine("Burger llegó!");
+            return burger;
+        }
+    }
+
+    class Burger
+    {
+        public string Tipo { get; set; }
+        public Burger()
+        {
+            Tipo = "Cheese Burger";
+        }
+    }
+}
